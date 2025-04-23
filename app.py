410dca25a0302bbe700aa517fc79fd9654be8607
cuervodetinta <ghstt.dante@gmail.com>
2025-04-23 14:38:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,12 @@ from PIL import Image as Image, ImageOps as ImagOps
 from keras.models import load_model
 import platform
 
-# Configuración de página
 st.set_page_config(
     page_title="Reconocimiento de Imágenes",
     page_icon="📷",
     layout="centered"
 )
 
-# CSS personalizado
 st.markdown(
     """
     <style>
@@ -45,23 +43,19 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Mostrar versión de Python
 st.write("Versión de Python:", platform.python_version())
 
-# Cargar el modelo
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-# Título
-st.title("Reconocimiento de Imágenes")
+st.title("RECONOCIMIENTO DE IMÁGENES")
 
-# Imagen de encabezado centrada
+
 image = Image.open('wiwiwi.png')
 st.markdown('<div class="centered-img">', unsafe_allow_html=True)
 st.image(image, width=350, use_container_width=False)
 st.markdown('</div>', unsafe_allow_html=True)
 
-# Input desde cámara
 img_file_buffer = st.camera_input("Toma una Foto")
 
 if img_file_buffer is not None:
